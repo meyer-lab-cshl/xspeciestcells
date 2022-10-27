4a523b4665e8558d10585cfa59f809fee7719926
salomecarcy <shalom.carcy@gmail.com>
2022-10-27 14:59:51 -0400

    cleanup

--- scripts/mouse_thymus/02_CreateShinyApp.R
+++ scripts/mouse_thymus/02_CreateShinyApp.R
@@ -12,8 +12,3 @@ library(ShinyCell)
 scConf = createConfig(seur.harmony)
 makeShinyApp(seur.harmony, scConf, gene.mapping = TRUE,
              shiny.title = "ShinyCell Mouse iNKT+Thymocyte data") 
-
-filtered_seurat_MNN <- FindVariableFeatures(filtered_seurat_MNN) # return 2,000 HVGs
-scConf_mnn = createConfig(filtered_seurat_MNN)
-makeShinyFiles(filtered_seurat_MNN, scConf_mnn, gex.assay="RNA",
-               shiny.prefix="fastMNN", shiny.dir="shinyAppHumaniNKT")

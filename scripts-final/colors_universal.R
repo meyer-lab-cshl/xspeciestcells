7da4c569563accccbf6ce0410d9b8d6707b38dab
salomecarcy <shalom.carcy@gmail.com>
2023-06-05 21:23:43 -0400

    script for universal colors

---
+++ scripts-final/colors_universal.R
@@ -0,0 +1,19 @@
+###
+# Purpose: Define colors for every figure/plot
+# Date: June 2023
+# Author: Salomé Carcy
+###
+
+
+# Integrated UMAP clusters
+cols_integrated <- c("0" = "#f4c40f", "1" = "#b75347", "2" = "#d8443c", "3" = "#e09351", "4" = "#2b9b81", 
+                     "5" = "#421401", "6" = "#92c051", "7" = "#9f5691", "8" = "#17154f", "9" = "#74c8c3", 
+                     "10" = "#5a97c1", "11" = "gold", "12" = "#a40000", "13" = "#72bcd5", "14" = "grey50",
+                     "15" = "orange", "16" = "blueviolet", "17" = "#0a2e57", "18" = "#bdbdbd")
+
+# Cell lineages
+cols_lineages <- c("CD4"  = "#74c476",
+                   "CD8"  = "#df65b0",
+                   "GD"   = "#08519c",
+                   "MAIT" = "#9ecae1",
+                   "NKT"  = "#9e9ac8")

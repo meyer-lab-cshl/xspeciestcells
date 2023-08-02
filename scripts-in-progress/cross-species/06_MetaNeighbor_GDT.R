fea0354b8897211aca7708df629916ee1e58c80f
salomecarcy <shalom.carcy@gmail.com>
2023-08-01 21:27:11 -0400

    cross-species GD: update with Lee data

--- scripts-in-progress/cross-species/06_MetaNeighbor_GDT.R
+++ scripts-in-progress/cross-species/06_MetaNeighbor_GDT.R
@@ -10,12 +10,13 @@ library(RColorBrewer)
 library(tidyverse)
 
 # Import data
-seur.ms <- readRDS("./data/cross-species/04_Metaneighbor_gdt/seuratobj_ms_gdt_sagar.rds")
-order_mouse <- c("cKIT+ DN1", "DN2", "DN3", "Pre-selected GD", "Post-selected GD", "Pan GD (mainly CD24+)", "CD122+ GD", "CD24- GD")
-cols_gdt <- brewer.pal(8, "Blues")
-names(cols_gdt) <- unique(seur.ms$cell.ident)
+seur.ms <- readRDS("./data/cross-species/00_Reproduce_UMAPs/ms_gdt_seurobj_lee.rds")
+# order_mouse <- c("cKIT+ DN1", "DN2", "DN3", "Pre-selected GD", "Post-selected GD", "Pan GD (mainly CD24+)", "CD122+ GD", "CD24- GD")
+# order_mouse <- c("Tγδp", "immature Tγδ1/17", "Tγδ1", "immature Tγδ17", "Tγδ17")
+cols_gdt <- brewer.pal(length(unique(seur.ms$gd_clusters)), "Blues")
+names(cols_gdt) <- unique(seur.ms$gd_clusters)
 SCpubr::do_DimPlot(seur.ms, 
-                   group.by = "cell.ident",
+                   group.by = "gd_clusters",
                    label=T,
                    label.color="black",
                    legend.position = "none",
@@ -23,7 +24,7 @@ SCpubr::do_DimPlot(seur.ms,
                    plot.title = "Mouse",
                    colors.use=cols_gdt,
                    font.size = 24)
-# ggsave("./data/cross-species/04_Metaneighbor_gdt/ms_umap.jpeg", width=6, height=6)
+# ggsave("./data/cross-species/04_Metaneighbor_gdt/ms_umap_lee.jpeg", width=6, height=6)
 
 
 seur.hu <- readRDS("./data/human-thymus/HumanData_12_AnalysisByLineage/thymus.GD.03_09_23.RDS")
@@ -49,69 +50,73 @@ length(unique(ortholog.df$hu_symbol)) # 17,152 hu genes
 
 
 # Get counts, HVGs and metadata
-ms.hvg <- VariableFeatures(FindVariableFeatures(seur.ms))
+ms.hvg <- VariableFeatures(FindVariableFeatures(seur.ms, nfeatures=2000))
+length(ms.hvg)
 ms.counts <- seur.ms[["RNA"]]@counts
 ms.metadata <- seur.ms@meta.data
 
+# hu.hvg <- VariableFeatures(seur.hu)
 hu.hvg <- VariableFeatures(seur.hu)
+length(hu.hvg)
 hu.counts <- seur.hu[["RNA"]]@counts
 hu.metadata <- seur.hu@meta.data
 
 
 # First, check whether genes can all be found in the ortholog table (many can't be found because I removed genes with orthology confidence=0)
-table(unique(rownames(ms.counts)) %in% ortholog.df$ms_symbol_data) # 16,819 not
-table(unique(rownames(hu.counts)) %in% ortholog.df$hu_symbol) # 4747 not
+table(unique(rownames(ms.counts)) %in% unique(ortholog.df$ms_ensemblID)) # 12,401 not
+table(unique(rownames(hu.counts)) %in% ortholog.df$hu_symbol) # 4,747 not
 
 
 # Subset the ortholog table to only genes that we can "translate"
 dictionary <- ortholog.df %>%
   as_tibble() %>%
   ## Intersection
-  filter(ms_symbol_data %in% unique(rownames(ms.counts)) & hu_symbol %in% unique(rownames(hu.counts))) %>%
-  ## Union
-  # filter(ms_symbol_data %in% unique(rownames(ms.counts)) | hu_symbol %in% unique(rownames(hu.counts))) %>%
-  # filter(hu_orthology_confidence == 1) %>%
+  filter(ms_ensemblID %in% unique(rownames(ms.counts)) & hu_symbol %in% unique(rownames(hu.counts))) %>%
   ## Remove any symbols that are NAs
-  filter(!is.na(ms_symbol_data)) %>%
+  filter(!is.na(ms_ensemblID)) %>%
   filter(!is.na(hu_symbol)) %>%
   ## Keep only 1:1 orthologs
-  group_by(ms_symbol_data) %>% filter(n_distinct(hu_symbol) == 1) %>% ungroup() %>%
-  group_by(hu_symbol) %>% filter(n_distinct(ms_symbol_data) == 1) %>% ungroup()
+  group_by(ms_ensemblID) %>% filter(n_distinct(hu_symbol) == 1) %>% ungroup() %>%
+  group_by(hu_symbol) %>% filter(n_distinct(ms_ensemblID) == 1) %>% ungroup()
+dim(dictionary) # 11,619 genes
 
 
 # Translate the mouse HVGs into "human gene" language
-ms.hvg.translated <- pull(dictionary %>% filter(ms_symbol_data %in% ms.hvg), # not all ms HVGs are found in ortholog.df
+ms.hvg.translated <- pull(dictionary %>% filter(ms_ensemblID %in% ms.hvg), # not all ms HVGs are found in ortholog.df
                           hu_symbol)
 hu.hvg.translated <- pull(dictionary %>% filter(hu_symbol %in% hu.hvg), # not all hu HVGs are found in ortholog.df
                           hu_symbol)
 total.hvg <- unique(union(ms.hvg.translated, hu.hvg.translated))
-length(total.hvg) # 1,965 genes
+length(total.hvg) # 2,002 genes (if 2000 HVG each) / 4872 (if 5000 HVGs each)
+table(total.hvg %in% unique(ms.hvg.translated), useNA="ifany") # 1,230 come from mouse
+table(total.hvg %in% unique(hu.hvg.translated), useNA="ifany") # 1,164 come from human
 
 
 # Keep only ms and hu genes that have 1:1 orthologs
-table(unique(rownames(ms.counts)) %in% dictionary$ms_symbol_data) # 11,914 genes should have a translation
-table(unique(rownames(hu.counts)) %in% dictionary$hu_symbol) # 11,914 genes should have a translation
-ms.counts <- ms.counts[rownames(ms.counts) %in% dictionary$ms_symbol_data,]
+table(unique(rownames(ms.counts)) %in% dictionary$ms_ensemblID) # 11,331 genes should have a translation
+table(unique(rownames(hu.counts)) %in% dictionary$hu_symbol) # 11,331 genes should have a translation
+ms.counts <- ms.counts[rownames(ms.counts) %in% dictionary$ms_ensemblID,]
 hu.counts <- hu.counts[rownames(hu.counts) %in% dictionary$hu_symbol,]
+nrow(ms.counts)==nrow(hu.counts) # should be TRUE
 
 
 # Translate the mouse genes in count table into "human gene"
 ms.dict <- dictionary %>%
-  filter(ms_symbol_data %in% rownames(ms.counts)) %>%
-  select(ms_symbol_data, hu_symbol, hu_orthology_confidence) %>%
+  filter(ms_ensemblID %in% rownames(ms.counts)) %>%
+  select(ms_ensemblID, hu_symbol, hu_orthology_confidence) %>%
   # distinct() %>%
-  # group_by(ms_symbol_data) %>% filter(n_distinct(hu_symbol)>1)
-  distinct(ms_symbol_data, .keep_all=T)
-ms.dict <- ms.dict[match(rownames(ms.counts), ms.dict$ms_symbol_data),]
-table(ms.dict$ms_symbol_data == rownames(ms.counts)) # all true
-table(is.na(ms.dict$hu_symbol)) # no NAs
+  # group_by(ms_ensemblID) %>% filter(n_distinct(hu_symbol)>1)
+  distinct(ms_ensemblID, .keep_all=T)
+ms.dict <- ms.dict[match(rownames(ms.counts), ms.dict$ms_ensemblID),]
+table(ms.dict$ms_ensemblID == rownames(ms.counts)) # should be all TRUE
+table(is.na(ms.dict$hu_symbol)) # should have no NAs
 # Translate
 rownames(ms.counts) <- ms.dict$hu_symbol
 
 
 # Merge everything into one
 ms.metadata$study <- "Mouse"
-ms.metadata <- ms.metadata[,c("cell.ident", "study")]
+ms.metadata <- ms.metadata[,c("gd_clusters", "study")]
 colnames(ms.metadata)[1] <- "clusters_GDT"
 head(ms.metadata)
 
@@ -128,6 +133,9 @@ seur.total <- merge(ms.seur, hu.seur)
 head(seur.total@meta.data)
 table(seur.total$clusters_GDT, useNA="ifany")
 table(seur.total$study, useNA="ifany")
+table(total.hvg %in% rownames(seur.total), useNA="ifany") # all HVGs should be found in the gene names of the count data
+grep("ENSMUS", rownames(seur.total), value=T) # check there are no ENSEMBLIDs in gene names
+
 
 
 # Convert seurat count matrix to SummarizedExperiment object
@@ -146,19 +154,21 @@ mtn <- MetaNeighborUS(var_genes=total.hvg,
 mtn <- readRDS("./data/cross-species/04_Metaneighbor_gdt/gdt_mssagar-hu_mtnslowversion_2023-07-10.rds")
 
 # Heatmap
-mtn.sub <- mtn[1:8,9:16]
+mtn.sub <- mtn[1:5,6:13]
 mtn.sub <- mtn.sub[,paste0("Human|GD_c", 0:7)]
 mtn.sub <- mtn.sub[paste0("Mouse|", order_mouse),]
 
-jpeg("./data/cross-species/04_Metaneighbor_gdt/gdt_mssagar-hu_slowversion_fulltree2.jpeg",
+jpeg("./data/cross-species/04_Metaneighbor_gdt/gd_hvg_test/fulltree_subcluters_hvg2000_intersect392.jpeg",
      width=1500, height=1500, res=200)
+# jpeg("./data/cross-species/04_Metaneighbor_gdt/gdt_mslee-hu_slowversion_fulltree_subclusters.jpeg",
+#      width=1500, height=1500, res=200)
 heatmap.2(mtn, # mtn.sub[,order(colnames(mtn.sub))],
           # trace
           trace="none",
           # dendrogram
-          # Rowv=FALSE,
-          # Colv=FALSE,
-          # dendrogram="none",
+          Rowv=FALSE,
+          Colv=FALSE,
+          dendrogram="none",
           # superimpose a density histogram on color key
           density.info="none",
           # color scale
@@ -168,7 +178,7 @@ heatmap.2(mtn, # mtn.sub[,order(colnames(mtn.sub))],
           key.title="",
           keysize = 1.2,
           # text labels
-          main="Mouse vs Human GDT",
+          main="Mouse vs Human GDT (intersect: 392 HVGs)",
           cexRow=0.6,
           cexCol=0.6,
           # margins
@@ -198,7 +208,7 @@ mtn.df <- mtn.df %>%
          propcells_human = ncells_human*100/totalcells_human) %>%
   # add nb of cells per mouse cluster
   dplyr::rename(Var1=Var2) %>%
-  left_join(as.data.frame(table(seur.ms$cell.ident)), by="Var1") %>%
+  left_join(as.data.frame(table(seur.ms$gd_clusters)), by="Var1") %>%
   dplyr::rename(mouse=Var1, ncells_mouse=Freq) %>%
   mutate(totalcells_mouse = dim(seur.ms)[2],
          propcells_mouse = ncells_mouse*100/totalcells_mouse)
@@ -206,7 +216,7 @@ mtn.df <- mtn.df %>%
 
 
 # PROPORTION OF HUMAN GDT CELLS IN EACH CLUSTER
-bp.x <- ggplot(data=mtn.df%>% select(human,propcells_human) %>% distinct(),
+bp.x <- ggplot(data=mtn.df %>% select(human,propcells_human) %>% distinct(),
                aes(x=factor(human, levels=paste0("GD_c", 0:7)), y=propcells_human))+
   geom_bar(stat="identity", fill="#bdbdbd") + theme_cowplot()+
   scale_x_discrete(position="top")+
@@ -222,7 +232,8 @@ bp.x <- ggplot(data=mtn.df%>% select(human,propcells_human) %>% distinct(),
 
 # PROPORTION OF MOUSE GDT CELLS IN EACH CLUSTER
 bp.y <- ggplot(data=mtn.df%>% select(mouse,propcells_mouse) %>% distinct(),
-               aes(x=factor(mouse, levels=rev(order_mouse)), y=propcells_mouse))+
+               # aes(x=factor(mouse, levels=rev(order_mouse)), y=propcells_mouse))+
+               aes(x=factor(mouse, levels=sort(unique(seur.ms$gd_clusters), decreasing=T)), y=propcells_mouse))+
   geom_bar(stat="identity", fill="#bdbdbd") +
   scale_x_discrete(position="top") +
   scale_y_continuous(limits=c(0,100), breaks=c(0,50,100))+
@@ -237,7 +248,8 @@ bp.y <- ggplot(data=mtn.df%>% select(mouse,propcells_mouse) %>% distinct(),
 # BUBBLE PLOT
 # library(scales)
 hm.clean <- ggplot(mtn.df, aes(x=factor(human, levels=paste0("GD_c", 0:7)),
-                               y=factor(mouse, levels=rev(order_mouse)))) +
+                               # y=factor(mouse, levels=rev(order_mouse)))) +
+                               y=factor(mouse, levels=sort(unique(seur.ms$gd_clusters), decreasing=T)))) +
   geom_point(aes(size = auroc, color= auroc))+
   geom_text(data=mtn.df %>% filter(auroc>0.65) %>% mutate(across("auroc", \(x) round(x,2))), aes(label=auroc), color="black")+
   scale_size_continuous(limits=c(0,1), breaks=seq(0,1, by=0.2), range = c(1, 15))+
@@ -250,4 +262,5 @@ hm.clean <- ggplot(mtn.df, aes(x=factor(human, levels=paste0("GD_c", 0:7)),
 # COMBINE
 library(patchwork)
 (bp.x+plot_spacer() + plot_layout(widths = c(5, 1))) / (hm.clean + bp.y + plot_layout(widths = c(5, 1))) + plot_layout(heights = c(1, 5))
-ggsave("./data/cross-species/04_Metaneighbor_gdt/gdt_ms-hu_metaneighbor_bubbleplot2.jpeg", width=12, height=9)
+ggsave("./data/cross-species/04_Metaneighbor_gdt/gdt_mslee-hu_metaneighbor_bubbleplot3.jpeg", width=12, height=9)
+# ggsave("./data/cross-species/04_Metaneighbor_gdt/gd_hvg_test/bubbleplot_subclusters_hvg2000_intersect392.jpeg", width=12, height=9)

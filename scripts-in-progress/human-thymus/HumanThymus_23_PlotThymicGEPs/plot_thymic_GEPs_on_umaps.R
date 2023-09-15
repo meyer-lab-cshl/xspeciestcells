a3e060eb22a7ec2af9d2812ff0a035a509a2887e
salomecarcy <shalom.carcy@gmail.com>
2023-09-15 19:18:06 -0400

    supp fig thymic GEPs on individual umaps

--- scripts-in-progress/human-thymus/HumanThymus_23_PlotThymicGEPs/plot_thymic_GEPs_on_umaps.R
+++ scripts-in-progress/human-thymus/HumanThymus_23_PlotThymicGEPs/plot_thymic_GEPs_on_umaps.R
@@ -42,6 +42,13 @@ seur.cd8@meta.data[,33:75]  <- NULL
 cnmf_gapin  <- read.csv("./data/human-PBMC/HumanData_27_ComparisonGEPsGarner/limited_nonimputed_genes_per_gep_post_rank_threshold_k12.csv", row.names=1)
 head(cnmf_gapin)
 
+# Import GEP usage
+gep_usage <- read.table("./data/human-PBMC/HumanData_20_RibbonPlotCellStateToID/cNMF_output/non_imputed_cNMF_allcells.usages.k_12.dt_0_02.consensus.txt", header=T)
+dim(gep_usage)
+colnames(gep_usage) <- paste0("GEP", c(2,5,3,1,4,12,6,7,8,10,9,11), "_usage") # rename column names
+head(gep_usage)
+gep_usage$GEP12_usage <- NULL
+
 
 
 # *****************
@@ -125,7 +132,7 @@ p_gdt  <- SCpubr::do_FeaturePlot(seur.gdt, features=geps_of_interest, ncol=1, le
   scale_colour_scico(palette = "lapaz", alpha = 0.8, begin = 0, end = 1, direction = -1)
 
 p_nkt | p_mait | p_gdt
-ggsave("./scripts-in-progress/human-thymus/HumanThymus_23_PlotThymicGEPs/plots/combined_umaps_innate.jpeg", width=18, height=35, limitsize=F)
+ggsave("./scripts-in-progress/human-thymus/HumanThymus_23_PlotThymicGEPs/plots/combined_umaps_innate.jpeg", width=20, height=35, limitsize=F)
 
 
 
@@ -133,7 +140,67 @@ ggsave("./scripts-in-progress/human-thymus/HumanThymus_23_PlotThymicGEPs/plots/c
 
 
 #___________________________
-## 3.4. Fourth analysis ####
+## 3.4. Plot GEP usage per lineage in grid ####
+
+# Split gep_usage to cells of interest
+gep_usage_nkt  <- gep_usage[rownames(gep_usage) %in% rownames(seur.nkt@meta.data), ]
+gep_usage_mait <- gep_usage[rownames(gep_usage) %in% rownames(seur.mait@meta.data), ]
+gep_usage_gdt  <- gep_usage[rownames(gep_usage) %in% rownames(seur.gdt@meta.data), ]
+gep_usage_cd4  <- gep_usage[rownames(gep_usage) %in% rownames(seur.cd4@meta.data), ]
+gep_usage_cd8  <- gep_usage[rownames(gep_usage) %in% rownames(seur.cd8@meta.data), ]
+
+# sanity checks
+table(rownames(seur.nkt@meta.data) ==rownames(gep_usage_nkt),  useNA="ifany")
+table(rownames(seur.mait@meta.data)==rownames(gep_usage_mait), useNA="ifany")
+table(rownames(seur.gdt@meta.data) ==rownames(gep_usage_gdt),  useNA="ifany")
+table(rownames(seur.cd4@meta.data) ==rownames(gep_usage_cd4),  useNA="ifany")
+table(rownames(seur.cd8@meta.data) ==rownames(gep_usage_cd8),  useNA="ifany")
+
+# Add GEP usage to seurat objects
+seur.nkt@meta.data  <- cbind(seur.nkt@meta.data, gep_usage_nkt)
+seur.mait@meta.data <- cbind(seur.mait@meta.data, gep_usage_mait)
+seur.gdt@meta.data  <- cbind(seur.gdt@meta.data, gep_usage_gdt)
+seur.cd4@meta.data  <- cbind(seur.cd4@meta.data, gep_usage_cd4)
+seur.cd8@meta.data  <- cbind(seur.cd8@meta.data, gep_usage_cd8)
+
+# Plot
+gepsusage_of_interest <- paste0("GEP", 1:11, "_usage")
+seur_vector <- list("NKT"=seur.nkt, "MAIT"=seur.mait, "GDT"=seur.gdt, "CD4"=seur.cd4, "CD8"=seur.cd8)
+
+plist_massive <- list()
+for(gep in gepsusage_of_interest){
+  print(gep)
+  # get row of plots (for one GEP)
+  plist <- list()
+  for(i_seur in names(seur_vector)){
+    print(i_seur)
+    seur <- seur_vector[[i_seur]]
+    # get legend once
+    if(gep=="GEP1_usage" & i_seur=="NKT"){
+      plegend <- ggpubr::get_legend(
+        SCpubr::do_FeaturePlot(seur,  features=gep, ncol=1, legend.position="right", legend.title="GEP usage", border.size=1, pt.size=2, order=T)+
+          scale_color_viridis_c(limits=c(0,1.25), option="B")+
+          theme(plot.background = element_rect(color = "black"))
+      )
+    }
+    # make plot
+    legendpos <- "none"
+    plt_title <- ""
+    if(gep=="GEP1_usage"){plt_title=i_seur}
+    p <- SCpubr::do_FeaturePlot(seur,  features=gep, ncol=1, legend.position=legendpos, border.size=1, pt.size=2, order=T, plot.title=plt_title)+
+      scale_color_viridis_c(limits=c(0,1.25), option="B")+
+      theme(plot.background = element_rect(color = "black", linewidth = 2),
+            plot.margin=unit(c(0,5.5,0,5.5), "points"))
+    plist[[i_seur]] <- ggrastr::rasterise(p, layers="Point", dpi=300)
+  }
+  prow_gep <- plot_grid(plotlist=plist, nrow=1, scale=0.9)
+  plist_massive[[gep]] <- prow_gep 
+}
+
+plot_grid(plot_grid(plotlist=plist_massive, ncol=1, align="h"),
+          ggpubr::as_ggplot(plegend), ncol=2, rel_widths = c(5, .5), scale=c(0.95, 5))
+# ggsave("./scripts-in-progress/human-thymus/HumanThymus_23_PlotThymicGEPs/plots/combined_umaps_gepusage3.pdf", width=25, height=40, limitsize=F, dpi=80)
+
 
 ## /end ####
 

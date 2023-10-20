639eb9f52ca9f2d3ac692098c54f5efc5a922b22
salomecarcy <shalom.carcy@gmail.com>
2023-10-20 10:07:04 -0400

    ms GD lee: convert gene symbols

--- scripts-in-progress/cross-species/00_MouseGDdataLee.R
+++ scripts-in-progress/cross-species/00_MouseGDdataLee.R
@@ -7,7 +7,6 @@
 # 1. IMPORT ####
 # **************
 
-# library(RaceID)
 library(Seurat)
 library(ggplot2)
 library(tidyverse)
@@ -62,7 +61,7 @@ DimPlot(seu.gdt17, reduction = "umap", group.by="seurat_clusters", label=T) + xl
 # table(rownames(seu@meta.data[rownames(seu@meta.data) %in% rownames(seu.gdt17@meta.data),]) == rownames(seu.gdt17@meta.data))
 seu@meta.data[rownames(seu@meta.data) %in% rownames(seu.gdt17@meta.data),"subclusters_gdt17"] <- seu.gdt17@meta.data$RNA_snn_res.0.4
 
-seu@meta.data$gd_clusters <- case_when(
+seu@meta.data$gd_subclusters <- case_when(
   seu@meta.data$res.0.8 == 0 ~ "G4 (immature Tγδ17)",
   seu@meta.data$res.0.8 == 1 & seu@meta.data$subclusters_gdt1 %in% 0:2 ~ "G7-1 (Tγδ1)",
   seu@meta.data$res.0.8 == 1 & seu@meta.data$subclusters_gdt1 == 3 ~ "G7-2 (Tγδ1)",
@@ -74,7 +73,25 @@ seu@meta.data$gd_clusters <- case_when(
   seu@meta.data$res.0.8 == 6 & seu@meta.data$subclusters_gdt17 == 1 ~ "G6-2 (Tγδ17)"
 )
 
+DimPlot(seu, reduction = "umap", group.by="gd_subclusters", label=T)
+
+
+# Make a column with higher level annotation
+seu@meta.data$gd_clusters <- case_when(
+  seu@meta.data$gd_subclusters == "G1 (Tγδp)"             ~ "Tγδp",
+  seu@meta.data$gd_subclusters == "G2 (immature Tγδ1/17)" ~ "immature Tγδ1/17",
+  seu@meta.data$gd_subclusters == "G3 (immature Tγδ1/17)" ~ "immature Tγδ1/17",
+  seu@meta.data$gd_subclusters == "G4 (immature Tγδ17)"   ~ "immature Tγδ17",
+  seu@meta.data$gd_subclusters == "G5 (immature Tγδ17)"   ~ "immature Tγδ17",
+  seu@meta.data$gd_subclusters == "G6-1 (Tγδ17)"          ~ "Tγδ17",
+  seu@meta.data$gd_subclusters == "G6-2 (Tγδ17)"          ~ "Tγδ17",
+  seu@meta.data$gd_subclusters == "G7-1 (Tγδ1)"           ~ "Tγδ1",
+  seu@meta.data$gd_subclusters == "G7-2 (Tγδ1)"           ~ "Tγδ1"
+)
 DimPlot(seu, reduction = "umap", group.by="gd_clusters", label=T)
+
+# sanity check
+# table(seu@meta.data[,c("gd_subclusters", "gd_clusters")], useNA="ifany")
 
 # Verify we have raw count data
 seu@assays$RNA@counts[6:10,1:5] # perfect!
@@ -82,7 +99,8 @@ seu@assays$RNA@counts[6:10,1:5] # perfect!
 
 # Keep only metadata of interest
 # colnames(seu@meta.data)
-cols_of_interest <- c("nCount_RNA", "nFeature_RNA", "pct_counts_Mt", "gd_clusters")
+cols_of_interest <- c("celltype", "newcelltype", "VA14JA18", "VB8", "VB7", "VA19JA33", "VB6", "VG123456", "ident", "res.0.8",
+                      "nCount_RNA", "nFeature_RNA", "pct_counts_Mt", "gd_clusters", "gd_subclusters")
 seu@meta.data <- seu@meta.data[,cols_of_interest]
 
 
@@ -93,3 +111,59 @@ seu@meta.data <- seu@meta.data[,cols_of_interest]
 # ************
 
 saveRDS(seu, "./data/cross-species/00_Reproduce_UMAPs/ms_gdt_seurobj_lee_subclusters.rds")
+
+
+
+
+# *******************************************
+# 4. CONVERT ENSEMBL IDs TO GENE SYMBOLS ####
+# *******************************************
+
+library(SingleCellExperiment)
+library(biomaRt)
+sce <- as.SingleCellExperiment(seu)
+
+# Get Mouse ENSEMBL - gene symbol correspondance from biomart
+ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
+
+genemap <- getBM(attributes=c('ensembl_gene_id', "external_gene_name"),
+                 filters = 'ensembl_gene_id',
+                 values = rownames(sce),
+                 mart = ensembl) %>%
+  dplyr::rename(gene_id = ensembl_gene_id,
+                symbol = external_gene_name) %>%
+  distinct()
+
+# how many gene names available?...
+table(rownames(sce) %in% genemap$gene_id, useNA="ifany") # 25,645
+
+# Change EMSEMBL IDs to gene symbols in SCE object
+featureData <- tibble(gene_id=rownames(sce)) %>%
+  left_join(genemap, by="gene_id") %>%
+  drop_na() %>%
+  filter(duplicated(symbol) == FALSE) %>%
+  filter(symbol != "")
+
+sce_final <- subset(sce, rownames(sce) %in% gene_id$gene_id)
+
+mcols(sce_final) <- DataFrame(mcols(sce_final), featureData)
+rownames(sce_final) <- rowData(sce_final)$symbol
+
+# convert back to seurat
+seu_final <- as.Seurat(sce_final)
+
+# add the dimension reductions that got lost
+seu_final@reductions$pca <- seu@reductions$pca
+seu_final@reductions$tsne <- seu@reductions$tsne
+seu_final@reductions$umap <- seu@reductions$umap
+seu_final@meta.data <- seu_final@meta.data[,2:16]
+
+DimPlot(seu_final, reduction="umap", group.by="gd_subclusters")
+rownames(seu_final)[1:5]
+
+# save for Laurent
+saveRDS(seu_final, "./data/cross-species/00_Reproduce_UMAPs/ms_gdt_seurobj_lee_genesymbols.rds")
+
+
+
+

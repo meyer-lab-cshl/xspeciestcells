f9dd0f3c98c7a4234e87d7f9ad9426132e7ccc58
HannahVMeyer <hannah.v.meyer@googlemail.com>
2023-11-09 15:28:54 -0500

    turn into function, analyse T_inn as well

--- scripts-in-progress/scenic/scenic_bubbleplot.R
+++ scripts-in-progress/scenic/scenic_bubbleplot.R
@@ -7,7 +7,7 @@ library(tidyverse)
 library(dplyr)
 library(readxl)
 library(RColorBrewer)
-
+library(patchwork)
 #############
 ## setup ####
 #############
@@ -24,6 +24,109 @@ colors <- tibble::tibble(attribute=c("TF", "cytokine", "chemokine", "other",
 colors_list <- as.list(colors$color)
 names(colors_list) <- colors$attribute
 
+#################
+## functions ####
+#################
+
+scenicBubble <- function(attributes, ordered) {
+  attributes_target <- attributes %>%
+    select(Target, Attributes) %>%
+    distinct
+  
+  if (ordered == "hierarchical") {
+    attributes_matrix <- as.matrix(table(attributes[,1:2]))
+    rowclust <- hclust(dist(attributes_matrix, "manhattan"))$order
+    colclust <- hclust(dist(t(attributes_matrix), "manhattan"))$order
+    
+    attributes_matrix <- attributes_matrix[rowclust,colclust]
+    
+    # Hierarchically clustered
+    attributes_df <- attributes_matrix %>%
+      as.data.frame %>%
+      mutate(Target= fct_inorder(Target),
+             TF=fct_inorder(TF)) %>%
+      filter(Freq != 0) %>%
+      left_join(attributes_target) %>%
+      as_tibble
+  } else {
+    # ordered by categories
+    attributes_df <- table(attributes[,1:2]) %>%
+      as.data.frame %>%
+      filter(Freq != 0) %>%
+      left_join(attributes_target) %>%
+      as_tibble
+  }
+  
+  tf_tf <- attributes_df  %>%
+    rowwise %>%
+    mutate(xy=paste(sort(c(as.character(TF), as.character(Target))), collapse="_")) %>%
+    group_by(xy) %>%
+    summarize(count = n()) %>%
+    filter(count > 1) %>%
+    ungroup() %>%
+    separate(xy, into = c("TF", "Target"))
+  
+  tf_tf <- tf_tf %>%
+    bind_rows(tibble(TF=tf_tf$Target, Target=tf_tf$TF, count=tf_tf$count))
+  
+  attributes_df <- attributes_df %>%
+    left_join(tf_tf) %>%
+    mutate(type= case_when(Attributes == "TF" & Target %in% unique(TF) ~ "TF - unidirectional",
+                           TRUE ~ "Other"),
+           type = case_when(!is.na(count) | TF == Target ~ "TF - bidirectional", 
+                            TRUE ~ type))
+  
+  # Proportion of genes in regulon
+  targets_per_tf <- attributes_df %>%
+    select(TF) %>%
+    group_by(TF) %>%
+    summarise(count=n())
+  
+  tf_per_target <- attributes_df %>%
+    select(Target) %>%
+    group_by(Target) %>%
+    summarise(count=n()) %>%
+    left_join(distinct(attributes_df[, c(2,4)])) %>%
+    arrange(Attributes, -count) %>%
+    mutate(Target= fct_inorder(Target))
+  
+  p_tf_per_target <- ggplot(tf_per_target,
+                            aes(y=count, x=Target, fill=Attributes)) +
+    geom_bar(stat="identity") +
+    scale_fill_manual(values=colors_list) +
+    scale_y_continuous(breaks=seq(0,12,2)) +
+    cowplot::theme_cowplot() +
+    labs(y="# TFs")+
+    theme(axis.text.x=element_blank(),
+          axis.title.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          legend.position = "none")
+  
+  if (ordered != "hierarchical") {
+    attributes_df <- attributes_df %>%
+      mutate(Target= factor(Target, levels=levels(tf_per_target$Target)),
+             TF=fct_inorder(TF)) 
+  }
+  
+  
+  p_reg <- ggplot(attributes_df)
+  p_reg <- p_reg +
+    geom_point(aes(x=Target, y=TF, color=type), size=4) +
+    labs(y="Transcription Factors",
+         x="Targets") +
+    scale_color_manual(values=c("darkgrey", "#7570b3",  "#1b9e77"))+
+    coord_fixed() +
+    cowplot::theme_cowplot() +
+    theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1),
+          legend.position = "bottom",
+          panel.grid.major = element_line(color="lightgrey", size=0.2),
+          panel.border = element_rect(color="black", linewidth=1),
+          axis.line = element_blank())
+  
+  
+  
+  p_tf_per_target /p_reg  + plot_layout(heights = unit(c(3, -1), c("cm", "null")))
+}
 
 ##########################
 ## INPUTS Description ####
@@ -50,19 +153,17 @@ names(colors_list) <- colors$attribute
 ################
 
 ## Regulons conventional T cells
-attributes <- read.csv("results/Pruned_Genes_Adaptive_regulons_w_attributes.csv")
+t_conv <- read.csv("results/Pruned_Genes_Conventional_regulons_w_attributes.csv")
+t_inn <- read.csv("results/Pruned_Genes_Innate_regulons_w_attributes.csv")
 
-attributes_TF_TF <- attributes %>%
-  filter(Target %in% unique(TF))
+p_conv <- scenicBubble(t_conv, ordered="count")
+p_inn <- scenicBubble(t_inn, ordered="count")
 
-attributes_uniqueTF <- attributes %>%
-  filter(!Target %in% unique(TF))
-
-attributes_matrix <- as.matrix(table(attributes[,1:2]))
-rowclust <- hclust(dist(attributes_matrix, "manhattan"))$order
-colclust <- hclust(dist(t(attributes_matrix), "manhattan"))$order
-
-attributes_matrix <- attributes_matrix[rowclust,colclust]
+ggsave(plot=p_conv, "results/scenic_Tconv.pdf",
+       width=9, height=7)
+ggsave(plot=p_inn, "results/scenic_Tinn.pdf",
+       width=9, height=7)
+####
 
 attributes_target <- attributes %>%
   select(Target, Attributes) %>%
@@ -74,90 +175,14 @@ attributes_tf <- attributes %>%
   rename(Attributes_TF=Attributes)
 
 attributes_TF_TF <- attributes %>%
+  filter(Target %in% unique(TF))
+
+attributes_uniqueTF <- attributes %>%
+  filter(!Target %in% unique(TF))
+
+attributes_TF_TF <- attributes %>%
   mutate(TF_TF = case_when(Target %in% unique(TF) ~ "TF",
                            TRUE ~ "other"))
-
-attributes_df <- attributes_matrix %>%
-  as.data.frame %>%
-  mutate(Target= fct_inorder(Target),
-         TF=fct_inorder(TF)) %>%
-  filter(Freq != 0) %>%
-  left_join(attributes_target) %>%
-  as_tibble
-
-tf_tf <- attributes_df  %>%
-  rowwise %>%
-  mutate(xy=paste(sort(c(as.character(TF), as.character(Target))), collapse="_")) %>%
-  group_by(xy) %>%
-  summarize(count = n()) %>%
-  filter(count > 1) %>%
-  ungroup() %>%
-  separate(xy, into = c("TF", "Target"))
-
-tf_tf <- tf_tf %>%
-  bind_rows(tibble(TF=tf_tf$Target, Target=tf_tf$TF, count=tf_tf$count))
-
-attributes_df <- attributes_df %>%
-  left_join(tf_tf) %>%
-  mutate(type= case_when(Attributes == "TF" & Target %in% unique(TF) ~ "TF - unidirectional",
-                         TRUE ~ "Other"),
-         type = case_when(!is.na(count) | TF == Target ~ "TF - bidirectional", 
-                           TRUE ~ type)) %>%
-  mutate(Target= fct_inorder(Target),
-         TF=fct_inorder(TF)) 
-         
-
-p_reg <- ggplot(attributes_df)
-p_reg <- p_reg +
-  geom_point(aes(x=Target, y=TF, color=type), size=4) +
-  labs(y="Transcription Factors",
-       x="Targets") +
-  scale_color_manual(values=c("darkgrey", "#7570b3",  "#ec7014"))+
-  #coord_fixed() +
-  #cowplot::theme_cowplot() +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1),
-        legend.position = "bottom")
-
-# Proportion of genes in regulon
-targets_per_tf <- attributes_df %>%
-  select(TF) %>%
-  group_by(TF) %>%
-  summarise(count=n())
-  
-tf_per_target <- attributes_df %>%
-  select(Target) %>%
-  group_by(Target) %>%
-  summarise(count=n())
-
-p_tf_per_target <- ggplot(tf_per_target,
-               aes(y=count, x=Target)) +
-  geom_bar(stat="identity", fill="#bdbdbd") +
-  cowplot::theme_cowplot() +
-  labs(y="# TFs")+
-  theme(#axis.text = element_text(size=15),
-        axis.text.x=element_blank(),
-        #axis.title.y=element_text(size=20),
-        axis.title.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        legend.position = "none")
-
-p_target_per_tf <- ggplot(targets_per_tf,
-                          aes(x=count, y=TF)) +
-  geom_bar(stat="identity", fill="#bdbdbd") +
-  cowplot::theme_cowplot() +
-  labs(x="# Targets")+
-  theme(#axis.text = element_text(size=15),
-        axis.text.y=element_blank(),
-        #axis.title.x=element_text(size=20),
-        axis.title.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        legend.position = "none")
-
-library(patchwork)
-
-(p_tf_per_target +plot_spacer() + plot_layout(widths = c(5, 1)))/(p_reg + p_target_per_tf + plot_layout(widths = c(5, 1))) + plot_layout(heights = c(1, 5))
-####
 
 attributes_df <- attributes_df %>%
   mutate(type = case_when((tf_tf$Target == Target &  tf_tf$TF == TF) | 
